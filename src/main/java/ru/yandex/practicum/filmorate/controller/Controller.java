0c261ddc2418966da1f7507fdb786a87b8dcaa45
AkadiG1t <dubkowalex@yandex.ru>
2024-08-06 19:23:24 +0300

    Исправить ошибки в классе Controller

--- src/main/java/ru/yandex/practicum/filmorate/controller/Controller.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/Controller.java
@@ -3,12 +3,13 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
 
-public abstract class Controller <T>{
+public abstract class Controller<T> {
     public abstract Film create(Film film);
     public abstract T update(T t);
     public abstract Collection<T> getAllObject();
     public abstract Collection<Long> getUsersId();
-    protected long nextId () {
+
+    protected long nextId() {
         long nextId = getUsersId().stream()
                 .mapToLong(id -> id)
                 .max()

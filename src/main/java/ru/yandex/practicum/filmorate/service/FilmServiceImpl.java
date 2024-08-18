ca892a09273b18e835c4cc3a0dcde73e57b6b672
AkadiG1t <dubkowalex@yandex.ru>
2024-08-18 20:49:02 +0300

    Исправить ошибки в FilmService

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
@@ -6,9 +6,7 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.repository.FilmRepository;
-
 import java.util.Collection;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor

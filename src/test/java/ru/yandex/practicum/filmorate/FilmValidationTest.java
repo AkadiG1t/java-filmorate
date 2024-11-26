fbceffd18ad8c04044074de33fe27ae492c8df9b
AkadiG1t <dubkowalex@yandex.ru>
2024-11-27 02:03:06 +0300

    Исправить ошибку связанную с удалением интерфейса

--- src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.marker.Create;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.service.FilmServiceImpl;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.time.LocalDate;
 import java.util.Set;
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class FilmValidationTest {
 
     Validator validator;
-    private FilmServiceImpl filmService;
+    private FilmService filmService;
 
     @BeforeEach
     public void setup() {

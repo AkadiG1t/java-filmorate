6012bd44d9e0f4895e3149e0e35d48b23df9f27b
AkadiG1t <dubkowalex@yandex.ru>
2025-01-17 21:35:04 +0300

    Обновить зависимости

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.Length;
 import ru.yandex.practicum.filmorate.annotations.ReleaseDateAnnotation;
 import ru.yandex.practicum.filmorate.marker.Create;
 import ru.yandex.practicum.filmorate.marker.Update;
@@ -21,6 +23,7 @@ public class Film {
     long id;
     @NotBlank(groups = Create.class)
     String name;
+    @Length(max = 200)
     String description;
     @ReleaseDateAnnotation
     @NotNull(groups = Create.class)
